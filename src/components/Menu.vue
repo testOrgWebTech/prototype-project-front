0f54ccc3bbd1f0f3936ae08d184e976be46c1f3e
ChanwitSS <chanwit.sa@ku.th>
2021-09-09 14:02:15 +0700

    test dependencies

---
+++ src/components/Menu.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+}
+</script>
+
+<style scoped lang="scss">
+
--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+Vue.use(Buefy)
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
