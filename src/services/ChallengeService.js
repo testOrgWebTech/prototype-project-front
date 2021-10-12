7b193cd5703fcc8dc3202b154ed4da0870fb7df8
Gruszht6215 <kunkawee.s@ku.th>
2021-10-13 01:18:52 +0700

    add store's challenge&service's challenge

---
+++ src/services/ChallengeService.js
@@ -0,0 +1,15 @@
+import Axios from "axios"
+const api_endpoint = process.env.VUE_APP_ENDPOINT || "http://localhost:8000"
+
+
+export default {
+    async getChallengeById(id) {
+        try {
+            let res = await Axios.get(`${api_endpoint}/api/challenges/${id}`)
+            return res.data
+        } catch (e) {
+
+        }
+
+    }
+}
