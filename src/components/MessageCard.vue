c1fcc4247fc7d28e5f2be7d6d2d27a06e0e8b74b
jimmy <patchapon.r@ku.th>
2021-10-16 05:14:19 +0700

    fix newline won't render in message + add tabs

--- src/components/MessageCard.vue
+++ src/components/MessageCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div>
-      <article class="media">
+    <div class="card">
+      <article class="media card-content">
         <figure class="media-left">
           <p class="image is-64x64">
             <img src="https://bulma.io/images/placeholders/128x128.png">
@@ -9,41 +9,58 @@
         </figure>
         <div class="media-content">
           <div class="content">
-            <p>
-              <strong>{{ sender }}</strong> <small>@johnsmith</small> <small>31m</small>
+
+              <p>
+              <strong>{{ sender.name }}</strong> <small> <{{ sender.email }}></small> <small>{{ ago }}</small>
               <br>
-              {{ msg }}
+                <span style="white-space: pre;">{{ msg }}</span>
+
             </p>
+
           </div>
-          <nav class="level is-mobile">
-            <div class="level-left">
-              <a class="level-item">
-                <span class="icon is-small"><i class="fas fa-reply"></i></span>
-              </a>
-              <a class="level-item">
-                <span class="icon is-small"><i class="fas fa-retweet"></i></span>
-              </a>
-              <a class="level-item">
-                <span class="icon is-small"><i class="fas fa-heart"></i></span>
-              </a>
-            </div>
-          </nav>
-        </div>
-        <div class="media-right">
-          <button class="delete"></button>
         </div>
       </article>
     </div>
+
+<!--    <div class="card">-->
+<!--      <div class="card-content">-->
+<!--        <div class="media">-->
+<!--          <div class="media-left">-->
+<!--            <figure class="image is-48x48">-->
+<!--              <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">-->
+<!--            </figure>-->
+<!--          </div>-->
+<!--          <div class="media-content">-->
+<!--            <p class="title is-4">John Smith</p>-->
+<!--            <p class="subtitle is-6">@johnsmith</p>-->
+<!--          </div>-->
+<!--        </div>-->
+
+<!--        <div class="content">-->
+<!--          Lorem ipsum dolor sit amet, consectetur adipiscing elit.-->
+<!--          Phasellus nec iaculis mauris. <a>@bulmaio</a>.-->
+<!--          <a href="#">#css</a> <a href="#">#responsive</a>-->
+<!--          <br>-->
+<!--          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
   </div>
 
 </template>
 
 <script>
 export default {
-  props: ["sender",'msg']
+  props: ["sender",'msg','ago']
 }
 </script>
 
 <style lang="scss" scoped>
-
+.pre-formatted {
+  white-space: pre;
+}
+pre {
+  overflow-wrap: break-word;
+  overflow: hidden;
+}
 </style>
--- src/views/Message.vue
+++ src/views/Message.vue
@@ -1,22 +1,52 @@
 <template>
   <div>
+    <Topbar></Topbar>
 <!--    {{ messages }}-->
 <!--    <b-table :data="messages" :columns="columns"></b-table>-->
     <div>
       <button @click="logAll()">LOG</button>
+      <button @click="currentMenu = 0">Inbox</button>
+      <button @click="currentMenu = 1">Sent</button>
     </div>
-    <div class="container">
-      <MessageCard v-for="(item) in this.messages"
-      :sender="item.sender.name" :msg="item.message" class="is-vcentered is-centered"></MessageCard>
-    </div>
+
+<!--    <b-tabs v-model="activeTab" :multiline="multiline">-->
+<!--      <template v-for="tab in tabs">-->
+<!--        <b-tab-item-->
+<!--            v-if="tab.displayed"-->
+<!--            :key="tab.id"-->
+<!--            :value="tab.id"-->
+<!--            :label="tab.label">-->
+<!--          {{ tab.content }}-->
+<!--        </b-tab-item>-->
+<!--      </template>-->
+<!--    </b-tabs>-->
+    <b-tabs v-model="activeTab" position="is-centered">
+      <b-tab-item label="Inbox">
+        <div class="container">
+          <MessageCard v-for="(item) in this.messages"
+                       :sender="item.sender" :msg="item.message" :ago="item.ago" class="is-vcentered is-centered"></MessageCard>
+        </div>
+      </b-tab-item>
+
+      <b-tab-item label="Sent">
+        <div class="container">
+          <MessageCard v-for="(item) in this.sentMessages"
+                       :sender="item.sender" :msg="item.message" class="is-vcentered is-centered">
+
+          </MessageCard>
+        </div>
+      </b-tab-item>
+
+    </b-tabs>
+
+
+
+
 
     <div>
       <MessagePost></MessagePost>
     </div>
-    <div class="container">
-      <MessageCard v-for="(item) in this.sentMessages.data"
-                   :sender="item.sender.name" :msg="item.message" class="is-vcentered is-centered"></MessageCard>
-    </div>
+
   </div>
 
 
@@ -28,19 +58,23 @@ import MessageCard from "../components/MessageCard";
 import MessagePost from "../components/MessagePost";
 import AuthUser from "../store/AuthUser";
 import AuthService from "../services/AuthService";
+import Topbar from "../components/Topbar";
 export default {
   components: {
     MessageCard,
-    MessagePost
+    MessagePost,
+    Topbar
   },
   data() {
     return {
-      messages: [
-
-      ],
-      sentMessages: [
-
-      ],
+      messages: [],
+      sentMessages: [],
+      currentMenu: 0,
+        activeTab: 0,
+      //   showMusic: true,
+      //   showBooks: false,
+      //   multiline: true
+      // }
     }
   },
   created() {
