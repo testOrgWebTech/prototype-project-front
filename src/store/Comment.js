83694d5ab0820c3e81ba4e567f94ee198d27dacc
ChanwitSS <chanwit.sa@ku.th>
2021-10-17 21:03:04 +0700

    add feature comment to post

---
+++ src/store/Comment.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+Vue.use(Vuex)
+
+const api_endpoint = process.env.VUE_APP_ENDPOINT || "http://localhost:8000"
+
+export default new Vuex.Store({
+    state: {
+        comments: []
+    },
+    mutations: {
+        fetchComment(state, res) {
+            state.comments = res.data;
+        },
+        newComment(state, res) {
+            console.log(res)
+            state.comments.push(res.data);
+        },
+        getCommentsByPostId(state, res) {
+            state.comments = res.data;
+        }
+    },
+    actions: {
+        async fetchComment({ commit }) {
+            const res = await axios.get(api_endpoint + '/api/comments', /*header*/);
+            commit('fetchComment', res);
+        },
+        async newComment({ commit }, payload) {
+            const res = await axios.post(api_endpoint + '/api/comments', payload, /*header*/);
+            commit('newComment', res);
+        },
+        async getCommentsByPostId({ commit }, post_id) {
+            const res = await axios.post(api_endpoint + '/api/comments-by-post-id/' + post_id, /*header*/);
+            console.log(res)
+            commit('getCommentsByPostId', res);
+        }
+    },
+    getters: {
+        comments: (state) => state.comments,
+    },
+    modules: {
+    }
+})
